d4cf68a158947b06bbf23a872a78073a32431bf9
Hamdi Boukamcha <hamdouchhd@hotmail.com>
2014-12-29 23:20:11 +0100

    Signature Recognition
    Signature Recognition [Neural Networks ] V4 : Simple and Effective
    Source Code Neural Networks Based Signature Recognition.

---
+++ sourcecode.m
@@ -0,0 +1,21 @@
+% 
+% NEURAL NETWORKS BASED SIGNATURE RECOGNITION
+% 
+% In order to obtain the complete source code for Neural Networks Based
+% Ssignature Recognition please visit my website
+% 
+% 
+% http://matlab-recognition-code.com/signature-recognition-based-on-neural-networks/
+%
+% For more informations please email me hamdouchhd@hotmail.com
+%
+% Hamdi Boukamcha
+% Tunisia
+% Sousse
+% 4081 
+% email  hamdouchhd@hotmail.com
+% mobile +216 50674269
+% website http://matlab-recognition-code.com
+%
+%
+%
